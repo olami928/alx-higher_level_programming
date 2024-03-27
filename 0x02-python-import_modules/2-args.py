4be9d3999859c4c9a5042dd0bd910de55ff535f5
olami928 <oladimejialabi45@gmail.com>
2024-03-26 18:03:16 -0700

    hot fix

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    i = len(sys.argv) - 1
+
+    if i == 0:
+    print("{} arguments".format(i))
+    if i == 1
+    print("{} argumets".format(i))
+    else:
+        print("{} arguments".format(i))
+
+    if i > 0:
+        for args in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, args))
+        args += 1
