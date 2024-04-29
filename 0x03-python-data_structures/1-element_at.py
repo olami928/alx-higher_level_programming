1404a1ca22e25b3b125b9cc3a020650739daf304
olami928 <olami928>
2024-04-29 17:13:46 +0100

    hot fix

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 # his program prints a list at a particular index
-if __name__ == "__main__":
-    def element_at(my_list, idx):
-        if idx < 0 or idx > len(my_list):
-            return(None)
-        else:
-            return(my_list[idx])
+def element_at(my_list, idx):
+    if idx < 0 or idx > len(my_list):
+        return(None)
+    else:
+        return(my_list[idx])
