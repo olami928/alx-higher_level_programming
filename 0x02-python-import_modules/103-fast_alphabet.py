6329082b1bc206e452edd584bf50c84862613777
olami928 <olami928>
2024-04-29 16:00:53 +0100

    hot fix

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import string
+    upper = list(string.ascii_uppercase)
+    print(upper)
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
     import hidden_4
     for n in dir(hidden_4):
         if n[0] != '_' and n[1] != '_':
-            print(name)
+            print(n)
