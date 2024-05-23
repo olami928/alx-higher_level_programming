5ae18c77e5e50a4800b0ddb9d6d7305008148ad7
olami928 <oladimejialabi45@gmail.com>
2024-05-23 15:28:11 -0700

    fixed task 1 and 2

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+-- this program creates a databases and checks if it exists
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+-- this script deletes a database and checks if it exists
+DROP DATABASE IF EXISTS hbtn_0c_0;
