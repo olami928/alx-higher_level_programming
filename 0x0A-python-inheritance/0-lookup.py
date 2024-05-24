f5ab073a755ddccc46a032c4cbfd19531e0cedf5
olami928 <oladimejialabi45@gmail.com>
2024-05-23 19:33:09 -0700

    fixed task 1

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # this program eturns the list of available attributes and methods of an object
+"""This program returns the list of avaulable methods of an object."""
 def lookup(obj):
     """ This program list available attribue of an object
 
