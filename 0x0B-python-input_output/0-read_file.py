9828af4b28da206a3fa6d5aecc697291f9b7dd25
olami928 <oladimejialabi45@gmail.com>
2024-05-31 04:01:49 -0700

    fixed task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """This code reada a text file and prints it to stdout"""
 
+
 def read_file(filename=""):
+    """This is the code that read a text file and prints it to stdout"""
 
     with open(filename, 'r', encoding='UTF-8') as f:
         for line in f:
