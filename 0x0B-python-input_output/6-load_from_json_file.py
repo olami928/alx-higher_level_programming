e432618abb88d96e49347179b47aeabcf6c80265
olami928 <oladimejialabi45@7-add_item.pygmail.com>
2024-06-12 17:51:09 +0100

    fixed task 7

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,4 +8,4 @@ import json
 def load_from_json_file(filename):
     """This code creates an object."""
     with open(filename, 'r', encoding='UTF-8') as f:
-        json.load(f)
+        return (json.load(f))
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""This function that creates an Object from a “JSON file”"""
+
+
+import json
+import os
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+jlist = []
+
+if os.path.exists(filename):
+    jlist = load_from_json_file(filename)
+
+for index in sys.argv[1:]:
+    jlist.append(index)
+save_to_json_file(jlist, filename)
