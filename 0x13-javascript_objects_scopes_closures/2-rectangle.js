56ca1b36d4dd017e70cd6b993f6726d9f6bb7694
olami928 <oladimejialabi45@7-add_item.pygmail.com>
2024-06-22 03:06:24 +0100

    hot fix

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
 class Rectangle {
-	constructor(w, h) {
-		if (w <= 0 || h <= 0) {
-			return {};
-		} else {
-			this.width = w;
-			this.height = h;
-		}
-	}
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return {};
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
 }
 
 module.exports = Rectangle;
