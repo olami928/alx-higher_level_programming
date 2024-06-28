a506de31753c590b9fbe96e95cd1ac6f8863f715
olami928 <oladimejialabi45@7-add_item.pygmail.com>
2024-06-28 19:00:18 +0100

    hot fix

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This code ists all states from the database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+    state_name = sys.argv[4]
+    cursor = db.cursor()
+    query = ("SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC".format(state_name))
+    cursor.execute(query)
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
