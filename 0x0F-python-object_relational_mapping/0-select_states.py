93d17bd4ebd13fd6c77796e38fca31d5a33615fb
olami928 <oladimejialabi45@7-add_item.pygmail.com>
2024-06-28 18:12:33 +0100

    hot fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,6 @@ import MySQLdb
 if __name__ == "__main__":
 
     db = MySQLdb.connect(
-            host="127.0.0.1",
             user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.argv[3]
