33e313bd77b0345400a3fbc2c5c29590f78487b1
olami928 <oladimejialabi45@7-add_item.pygmail.com>
2024-07-20 23:49:31 +0100

    fixed task 0 and 1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 # this script takes a URL, sends a request
 # to that URL, and displays the size of the body of the respons
 
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 # this script takes in a URL, sends a
 # GET request to the URL, and displays the body of the responsl
 
