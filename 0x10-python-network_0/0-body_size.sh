26cc38535ca52f81a562f28b8f31b36c962770f7
olami928 <oladimejialabi45@7-add_item.pygmail.com>
2024-07-20 23:47:51 +0100

    fixed task 0 and 1

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# this script takes a URL, sends a request
+# to that URL, and displays the size of the body of the respons
+
+curl -s "$1" | wc -c
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# this script takes in a URL, sends a
+# GET request to the URL, and displays the body of the responsl
+
+curl -sL "$1"
