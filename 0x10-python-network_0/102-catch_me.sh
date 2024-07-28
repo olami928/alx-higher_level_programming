5e230c4a63d6f2ce5b3db72c0e6485b76f4c24a4
olami928 <oladimejialabi45@7-add_item.pygmail.com>
2024-07-29 00:59:52 +0100

    hot fix

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # this is a bash script thatmakes a request to 0.0.0.0:5000/catch_me
-curl -s -L -X PUT -d "user_id=98" -H "Origin: AlxSchool" 0.0.0.0:5000/catch_me
+curl -s -X POST -H "Content-Type: application/json" -d \\
+'{"user_id":"98"}' http://0.0.0.0:5000/catch_me
