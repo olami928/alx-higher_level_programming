64a7d9e9e1822bda707afd8879415b313f5edf07
olami928 <oladimejialabi45@7-add_item.pygmail.com>
2024-08-07 19:41:35 +0100

    task 6 done

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,32 +5,32 @@ const request = require('request');
 const url = process.argv[2];
 
 if (!url) {
-	console.log('Usage ./6-complted_tasks.js <API URL>');
-	process.exit(1);
+  console.log('Usage ./6-complted_tasks.js <API URL>');
+  process.exit(1);
 }
 
 request.get(url, (error, response, body) => {
-	if (error) {
-		console.error(`Error: ${error.message}`);
-		return;
-	}
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
 
-	if (response.statusCode === 200) {
-		const todos = JSON.parse(body);
-		const userTaskCount = {};
+  if (response.statusCode === 200) {
+    const todos = JSON.parse(body);
+    const userTaskCount = {};
 
-		todo.forEach(todo => {
-			if (userTaskCount[todo.userId]) {
-				userTaskCoujt[todo.userId]++;
-			} else {
-				userTaskCount[todo.userId] = 1;
-			}
-		}
-	});
+    todos.forEach(todo => {
+      if (todo.completed) {
+        if (userTaskCount[todo.userId]) {
+          userTaskCount[todo.userId]++;
+        } else {
+          userTaskCount[todo.userId] = 1;
+        }
+      }
+    });
 
-		console.log(userTaskCount);
-	} else {
-		console.log(`Error: ${response.StatusCode}`);
-	}
+    console.log(userTaskCount);
+  } else {
+    console.log(`Error: ${response.statusCode}`);
+  }
 });
-	
